5127c6a36c3b34e8cd1becfdabc04c91c35a3fea
misaelo <misael.arroyo@hotmail.com>
2016-07-19 15:45:27 -0400

    cliente

---
+++ vista/cliente/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="HandheldFriendly" content="true" />
+    <link type="text/css" rel="stylesheet" href="../bootstrap/css/bootstrap.min.css"  media="screen,projection"/>
+	<script src="../js/jquery-1.11.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="../fullpage/css/jquery.fullPage.css" />
+	<script src="../fullpage/js/jquery.easings.min.js"></script>
+	<script type="text/javascript" src="../fullpage/js/scrolloverflow.min.js"></script>
+	<script type="text/javascript" src="../fullpage/js/jquery.fullPage.js"></script>
+
+	<title>MGTOPOGRAFIA</title>
+
+</head>
+<body>
+	<div class="container">
+		<h1>hola gay del edu</h1>
+	</div>
+</body>
+</html>
