7636042080b97b2640791fae3615db9a52f50a75
misaelo <misael.arroyo@hotmail.com>
2016-08-14 20:13:20 -0300

    ss

--- vista/cliente/index.php
+++ vista/cliente/index.php
@@ -4,14 +4,14 @@ require_once("../../conexion/bd.php");
 require_once("../../modelo/empresa_modelo.php");
 require_once("../../controlador/empresa_controlador.php");
 
-foreach(buscar_informacion() as $informacion){
+/*foreach(buscar_informacion() as $informacion){
 
-	/*echo $informacion['historia']; 
+	echo $informacion['historia']; 
 	echo $informacion['mision'];
 	echo $informacion['vision'];
 	
-//Para cada pagina tienes que hacer este procedimiento.*/
-	}
+//Para cada pagina tienes que hacer este procedimiento.
+	}*/
 
 
 ?>
